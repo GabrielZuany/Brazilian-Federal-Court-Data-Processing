e81fb6aef7b45bd0f66133c5055354a212681b1f
GabrielZuany <gzuanydev@gmail.com>
2023-10-17 17:41:13 -0300

    Instanceof test case

--- BFC_DataProcessing/src/App.java
+++ BFC_DataProcessing/src/App.java
@@ -1,17 +1,30 @@
+import java.util.Date;
+
 import bfcdp.candidate.Candidate;
 import bfcdp.candidate.FederalCandidate;
 import bfcdp.candidate.StateCandidate;
-import bfcdp.electoralparty.ElectoralParty;
-import bfcdp.BFCDataProcessing;
+import bfcdp.enums.EnumApplication;
+//import bfcdp.electoralparty.ElectoralParty;
+//import bfcdp.BFCDataProcessing;
+import bfcdp.enums.EnumGender;
+import bfcdp.enums.EnumResult;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Candidate candidate = new Candidate();
-        FederalCandidate federalCandidate = new FederalCandidate();
-        StateCandidate stateCandidate = new StateCandidate();
-        ElectoralParty electoralParty = new ElectoralParty();
-        BFCDataProcessing bfcDataProcessing = new BFCDataProcessing();
-        System.out.println("Hello, World!");
+        //Candidate candidate = new Candidate(null, null, null, null, null, null, null); // impossible to instantiate an abstract class
+        Candidate federalCandidate = 
+        new FederalCandidate("1", "0", new Date(), EnumGender.MALE, EnumResult.WIN, EnumApplication.APPROVED, null, 1456);
+
+        if(federalCandidate instanceof FederalCandidate){
+            System.out.println("yay");
+        }else{
+            System.out.println("nay");
+        }
+        
+        //StateCandidate stateCandidate = new StateCandidate();
+        //ElectoralParty electoralParty = new ElectoralParty();
+        //BFCDataProcessing bfcDataProcessing = new BFCDataProcessing();
+        //System.out.println("Hello, World!");
     }
 }
 
