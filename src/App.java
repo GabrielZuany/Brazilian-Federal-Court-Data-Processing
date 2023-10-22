20faaa27e7b38b76ba2e10d58ec78ecde6878efb
lorenzo fiorot <lorenzorfjogos123@gmail.com>
2023-10-22 03:22:06 -0300

    leitura fora da main

--- src/App.java
+++ src/App.java
@@ -25,173 +25,22 @@ public class App {
 
         //LEITURA CANDIDATOS E PARTIDOS
         try(FileInputStream fileCandidates = new FileInputStream(candidateQuery)){
-            Scanner scanner = new Scanner(fileCandidates, "ISO-8859-1");
-            boolean skip = true;
-
-            Candidate candidate = null;
-            String id = null;                      // NR_CANDIDATO
-            String ballotBoxId = null;             // NM_URNA_CANDIDATO
-            Date birthDate = null;                 // DT_NASCIMENTO
-            EnumGender gender = null;              // CD_GENERO
-            EnumResult result = null;              // CD_SIT_TOT_TURNO
-            EnumApplication application = null;    // CD_SITUACAO_CANDIDATO_TOT
-            EnumVoteType voteType = null;           // NM_TIPO_DESTINACAO_VOTOS
-            ElectoralParty electoralParty = null;
-            String electoralPartyId = null;        // NR_PARTIDO
-            String electoralPartyAcronym = null;   // SG_PARTIDO
-            String electoralPartyFederationId = null; // NR_FEDERACAO
-
-            while(scanner.hasNextLine()){
-                String line = scanner.nextLine();
-                int idx = 0;
-                line = line.replace("\"", "");
-                String[] columns = line.split(";");
-                
-                for (String items : columns) {
-                    if(skip){
-                        break;
-                    }
-                    if(idx == 13){
-                        if(items.equals("6") && candidateType == EnumCandidateType.FEDERAL){
-                            candidateType = EnumCandidateType.FEDERAL;
-                        }
-                        else if(items.equals("7") && candidateType == EnumCandidateType.STATE){
-                            candidateType = EnumCandidateType.STATE;
-                        }
-                        else{
-                            skip = true;
-                            break;
-                        }
-                    }
-                    if(idx == 16){
-                        id = items;
-                    }
-                    if(idx == 18){
-                        ballotBoxId = items;
-                    }
-                    if(idx == 27){
-                        electoralPartyId = items;
-                    }
-                    if(idx == 28){
-                        electoralPartyAcronym = items;
-                    }
-                    if(idx == 30){
-                        electoralPartyFederationId = items;
-                    }
-                    if(idx == 42){
-                        birthDate = new SimpleDateFormat("dd/MM/yyyy").parse(items);
-                    }
-                    if(idx == 45){
-                        gender = items.equals("2")? EnumGender.MALE : EnumGender.FEMALE;
-                    }
-                    if(idx == 56){
-                        result = items.equals("2") | items.equals("3")? EnumResult.WIN : EnumResult.LOSE;
-                    }
-                    if(idx == 67){
-                        voteType = items.equals("Válido (legenda)")? EnumVoteType.LEGENDA : EnumVoteType.NOMINAL;
-                    }
-                    if(idx == 68){
-                        application = (items.equals("2") | items.equals("16"))? EnumApplication.APPROVED : EnumApplication.REJECTED;
-                    }                    
-                    idx++;
-                }
-                if(!skip){
-                    electoralParty = sistemaEleitoral.getElectoralPartyById(electoralPartyId);
-                    if(electoralParty == null){
-                        electoralParty = new ElectoralParty(electoralPartyId, electoralPartyAcronym, electoralPartyFederationId);
-                        sistemaEleitoral.addElectoralParty(electoralParty);
-                    }
-                    
-
-                    //adicionar aqui a quantidade de candidatos eleitos do partido ou fazer a leitura toda vez q for printar
-
-                    if(candidateType == EnumCandidateType.FEDERAL){
-                        candidate = new FederalCandidate(id, ballotBoxId, birthDate, gender, result, application, voteType, electoralParty);
-                    }else{
-                        candidate = new StateCandidate(id, ballotBoxId, birthDate, gender, result, application, voteType, electoralParty);
-                    }
-
-                    sistemaEleitoral.addCandidate(candidate);
-                    //System.out.print(candidate.toStringDebug());
-                }
-                skip = false;
-            }
-        }
-        catch(Exception e){
+            sistemaEleitoral.LeCandidatos(fileCandidates, candidateType);
+        }catch(Exception e){
             System.out.println("Arquivo de entrada Candidatos inexistente!");
             e.printStackTrace();
         }
 
-        //LEITURA CANDIDATOS E PARTIDOS
-        String votableId = null;    //NR_VOTAVEL
-        int votes = 0;              //QT_VOTOS_NOMINAIS
+        //LEITURA VOTOS
         try(FileInputStream fileVotos =  new FileInputStream(votingSection)){
-            Scanner scanner = new Scanner(fileVotos, "ISO-8859-1");
-            boolean header = true;
-            boolean skip;
-            while(scanner.hasNextLine()){
-                skip = false;
-                String line = scanner.nextLine();
-
-                if(header){ header = false; continue; }
-                line = line.replace("\"", "");
-                String[] columns = line.split(";");
-                
-                int idx = 0;    
-                for (String items : columns) {
-                    if(idx == 17){
-                        if(items.equals("6") && candidateType == EnumCandidateType.FEDERAL){
-                            candidateType = EnumCandidateType.FEDERAL;
-                        }
-                        else if(items.equals("7") && candidateType == EnumCandidateType.STATE){
-                            candidateType = EnumCandidateType.STATE;
-                        }
-                        else{
-                            skip = true;
-                            break;
-                        }
-                    }
-                    if(idx == 19){
-                        votableId = items;
-                        if(items.equals("95") ||items.equals("96") ||items.equals("97") ||items.equals("98")){
-                            skip = true;
-                            break;
-                        }
-                    }
-                    if(idx == 21){
-                        votes = Integer.parseInt(items);
-                    }
-                    idx++;
-                }
-                
-                if(skip) continue;
-
-                Candidate c = sistemaEleitoral.getCandidate(votableId);
-                if(c != null){
-                    if((c.getVoteType() == EnumVoteType.NOMINAL) && (c.getApplication() == EnumApplication.APPROVED)){
-                        c.addVotes(votes);
-                        c.getElectoralParty().addVotesNominais(votes);
-                    }
-                    // else{
-                    //     c.getElectoralParty().addVotesLegenda(votes);
-                    // }
-                }else{
-                    ElectoralParty e = sistemaEleitoral.getElectoralPartyById(votableId); 
-                    if(e == null){
-                        System.out.println("Partido não encontrado!");
-                        break;
-                    }
-                    e.addVotesLegenda(votes);
-                }
-            }  
-            
-        }        
-        catch(Exception e){
+            sistemaEleitoral.LeVotos(fileVotos, candidateType);            
+        }catch(Exception e){
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
 
         //sistemaEleitoral.mostVotedCandidates();
+        sistemaEleitoral.numeroEleitos();
         sistemaEleitoral.mostVotedParties();
     }
 }
--- src/bfcdp/BFCDataProcessing.java
+++ src/bfcdp/BFCDataProcessing.java
@@ -1,17 +1,38 @@
 package bfcdp;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
+import java.text.SimpleDateFormat;
 
 import bfcdp.candidate.Candidate;
+import bfcdp.candidate.FederalCandidate;
+import bfcdp.candidate.StateCandidate;
 import bfcdp.electoralparty.ElectoralParty;
+import bfcdp.enums.EnumApplication;
+import bfcdp.enums.EnumCandidateType;
+import bfcdp.enums.EnumGender;
 import bfcdp.enums.EnumResult;
+import bfcdp.enums.EnumVoteType;
 
 public class BFCDataProcessing {
     private HashMap<String,Candidate> candidates = new HashMap<String,Candidate>();
     private List<ElectoralParty> electoralParties = new ArrayList<ElectoralParty>(); // switch to Set<>();
+    int numEleitos = 0;
     
+
+    public void addEleitos(){
+        this.numEleitos++;
+    }
+
+
+    public void numeroEleitos(){
+        System.out.println("Número de vagas: " + numEleitos);
+    }
+
     public List<ElectoralParty> getElectoralParties() {
         return new ArrayList<ElectoralParty>(electoralParties);
     }
@@ -127,5 +148,170 @@ public class BFCDataProcessing {
         return count;
     }
 
+
+    public void LeCandidatos(FileInputStream  fileCandidates, EnumCandidateType candidateType){
+        Scanner scanner = new Scanner(fileCandidates, "ISO-8859-1");
+        boolean skip = true;
+
+        Candidate candidate = null;
+        String id = null;                      // NR_CANDIDATO
+        String ballotBoxId = null;             // NM_URNA_CANDIDATO
+        Date birthDate = null;                 // DT_NASCIMENTO
+        EnumGender gender = null;              // CD_GENERO
+        EnumResult result = null;              // CD_SIT_TOT_TURNO
+        EnumApplication application = null;    // CD_SITUACAO_CANDIDATO_TOT
+        EnumVoteType voteType = null;           // NM_TIPO_DESTINACAO_VOTOS
+        ElectoralParty electoralParty = null;
+        String electoralPartyId = null;        // NR_PARTIDO
+        String electoralPartyAcronym = null;   // SG_PARTIDO
+        String electoralPartyFederationId = null; // NR_FEDERACAO
+
+
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            int idx = 0;
+            line = line.replace("\"", "");
+            String[] columns = line.split(";");
+            
+            for (String items : columns) {
+                if(skip){
+                    break;
+                }
+                if(idx == 13){
+                    if(items.equals("6") && candidateType == EnumCandidateType.FEDERAL){
+                        candidateType = EnumCandidateType.FEDERAL;
+                    }
+                    else if(items.equals("7") && candidateType == EnumCandidateType.STATE){
+                        candidateType = EnumCandidateType.STATE;
+                    }
+                    else{
+                        skip = true;
+                        break;
+                    }
+                }
+                if(idx == 16){
+                    id = items;
+                }
+                if(idx == 18){
+                    ballotBoxId = items;
+                }
+                if(idx == 27){
+                    electoralPartyId = items;
+                }
+                if(idx == 28){
+                    electoralPartyAcronym = items;
+                }
+                if(idx == 30){
+                    electoralPartyFederationId = items;
+                }
+                if(idx == 42){
+                    try{
+                        birthDate = new SimpleDateFormat("dd/MM/yyyy").parse(items);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                         System.out.println("Formato de data inválido!");
+                    }
+                }
+                if(idx == 45){
+                    gender = items.equals("2")? EnumGender.MALE : EnumGender.FEMALE;
+                }
+                if(idx == 56){
+                    result = items.equals("2") | items.equals("3")? EnumResult.WIN : EnumResult.LOSE;
+                }
+                if(idx == 67){
+                    voteType = items.equals("Válido (legenda)")? EnumVoteType.LEGENDA : EnumVoteType.NOMINAL;
+                }
+                if(idx == 68){
+                    application = (items.equals("2") | items.equals("16"))? EnumApplication.APPROVED : EnumApplication.REJECTED;
+                }                    
+                idx++;
+            }
+            if(!skip){
+                electoralParty = getElectoralPartyById(electoralPartyId);
+                if(electoralParty == null){
+                    electoralParty = new ElectoralParty(electoralPartyId, electoralPartyAcronym, electoralPartyFederationId);
+
+                    addElectoralParty(electoralParty);
+                }
+
+                if(candidateType == EnumCandidateType.FEDERAL){
+                    candidate = new FederalCandidate(id, ballotBoxId, birthDate, gender, result, application, voteType, electoralParty);
+                }else{
+                    candidate = new StateCandidate(id, ballotBoxId, birthDate, gender, result, application, voteType, electoralParty);
+                }
+
+                addCandidate(candidate);
+
+                if(candidate.getResult() == EnumResult.WIN){
+                    candidate.getElectoralParty().addCandidatesWin();
+                    addEleitos();
+                }
+            }
+            skip = false;
+        }
+    }
+
+    public void LeVotos(FileInputStream fileVotos, EnumCandidateType candidateType){
+        Scanner scanner = new Scanner(fileVotos, "ISO-8859-1");
+        String votableId = null;    //NR_VOTAVEL
+        int votes = 0;              //QT_VOTOS_NOMINAIS
+        boolean header = true;
+        boolean skip;
+        while(scanner.hasNextLine()){
+            skip = false;
+            String line = scanner.nextLine();
+
+            if(header){ header = false; continue; }
+            line = line.replace("\"", "");
+            String[] columns = line.split(";");
+            
+            int idx = 0;    
+            for (String items : columns) {
+                if(idx == 17){
+                    if(items.equals("6") && candidateType == EnumCandidateType.FEDERAL){
+                        candidateType = EnumCandidateType.FEDERAL;
+                    }
+                    else if(items.equals("7") && candidateType == EnumCandidateType.STATE){
+                        candidateType = EnumCandidateType.STATE;
+                    }
+                    else{
+                        skip = true;
+                        break;
+                    }
+                }
+                if(idx == 19){
+                    votableId = items;
+                    if(items.equals("95") ||items.equals("96") ||items.equals("97") ||items.equals("98")){
+                        skip = true;
+                        break;
+                    }
+                }
+                if(idx == 21){
+                    votes = Integer.parseInt(items);
+                }
+                idx++;
+            }
+            
+            if(skip) continue;
+
+            Candidate c = getCandidate(votableId);
+            if(c != null){
+                if((c.getVoteType() == EnumVoteType.NOMINAL) && (c.getApplication() == EnumApplication.APPROVED)){
+                    c.addVotes(votes);
+                    c.getElectoralParty().addVotesNominais(votes);
+                }
+                else if((c.getVoteType() == EnumVoteType.LEGENDA)){
+                    c.getElectoralParty().addVotesLegenda(votes);
+                }
+            }else{
+                ElectoralParty e = getElectoralPartyById(votableId); 
+                if(e == null){
+                    System.out.println("Partido não encontrado!");
+                    break;
+                }
+                e.addVotesLegenda(votes);
+            }
+        } 
+    }
 
 }
